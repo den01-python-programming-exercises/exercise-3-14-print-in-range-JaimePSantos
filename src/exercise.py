adbd04a40243ab0e18a58af2f257273a6c72e06d
Jaime Santos <jaimepereirasantos123@gmail.com>
2020-10-22 11:24:54 +0000

    solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,13 @@
 def main():
     #write your code below this line
+    numbers = []
+
+
+def print_numbers_in_range(numbers, lower_limit, upper_limit):
+  print("The numbers in the range [%s, %s]"%(lower_limit,upper_limit))
+  for number in numbers:
+    if(number>=lower_limit and number <= upper_limit):
+      print(number)
 
 if __name__ == '__main__':
     main()
